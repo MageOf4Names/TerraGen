f5ffe57fa43aef4d5f5ba13f0d38d82f503172f7
Sean Alberto <sean171035@gmail.com>
2021-03-21 16:35:54 -0600

    Default constructor with 4 players and a 10x10 square grid

--- src/Game.java
+++ src/Game.java
@@ -2,8 +2,20 @@ import java.awt.*;
 
 public class Game {
     public User[] users;
-
     public Map map;
+
+    public Game() {
+        // Default pool of 4 users
+        this.users = new User[] {
+                new User("user1"),
+                new User("user2"),
+                new User("user3"),
+                new User("host")
+        };
+
+        // Default map of a 10x10 square grid
+        this.map = new Map(new SquareTileGrid(10,10, 50));
+    }
 
     public Game(User[] users, Map map) {
         this.users = users;
