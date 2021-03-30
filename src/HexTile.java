c6c4f9d9807f50cf45418cd0a3aa06e8dc93bc3c
Douglas Newquist <deathsnake@protonmail.com>
2021-03-29 18:22:47 -0600

    Fixed hex tile center point

--- src/HexTile.java
+++ src/HexTile.java
@@ -9,36 +9,38 @@ public class HexTile extends Tile implements Serializable {
     @Override
     public Point getCenter(float scale) {
         float r = scale / 2;
-        /* Conditional branch for tiling offset (odd rows are offset by a radius) */
-        if (this.location.y % 2 == 0) {
-            return new Point(this.location.x * scale + r, this.location.y * scale + r);
-        } else {
-            return new Point((this.location.x + 1) * scale, this.location.y * scale + r);
-        }
+        int s = (int) (r * Math.sqrt(3));
+        return new Point(r, s);
+    }
+
+    @Override
+    public Point getPixelLocation(float scale) {
+        int x = (int) (location.x * scale);
+        int y = (int) (location.y * scale);
+
+        if ((location.y % 2) == 1)
+            x += (int) (scale / 2);
+
+        y *= Math.sqrt(3) / 2;
+
+        return new Point(x, y);
     }
 
     @Override
     public void draw(Graphics2D g, float scale) {
-        Point center = getCenter(scale);
+        Point center = getPixelCenterLocation(scale);
+        Point offset = getCenter(scale);
         var c = g.getColor();
 
         int r = (int) (scale / 2);
-        int x = center.x - r;
-        int y = center.y - r;
-        int s = (int) (scale / 1.73205);
-        int t = (int) (r / 1.73205);
-        int tmpX, tmpY;
+        int x = center.x;
+        int y = center.y;
 
-        int[] pX = {x+t,x+s+t,x+s+t+t,x+s+t,x+t,x};
-        int[] pY = {y,y,y+r,y+r+r,y+r+r,y+r};
+        int t = (int) (r / Math.sqrt(3));
+        int s = offset.y - t;
 
-        /* Rotates original points 90 degrees to tile correctly */
-        for (int i = 0; i < 6; i++) {
-            tmpX = pX[i] - center.x;
-            tmpY = pY[i] - center.y;
-            pX[i] = -1 * tmpY + center.x;
-            pY[i] = (int) (tmpX + center.y - this.location.y * 13 * scale / 80);
-        }
+        int[] pX = {x, x - r, x - r, x, x + r, x + r};
+        int[] pY = {y + s, y + t, y - t, y - s, y - t, y + t};
 
         // Creates a new polygon object to be drawn
         Polygon p = new Polygon(pX, pY, 6);
