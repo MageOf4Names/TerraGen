38e1d34812046f5372bdb9ee04f6a67346cf470e
MattDudek <matthew.dudek@student.nmt.edu>
2021-04-28 21:11:00 -0600

    new new logo

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -12,7 +12,7 @@ public class MainScreen extends JComponent {
         //Box.Filler filler = new Box.Filler(new Dimension(50,50), new Dimension(1280, 300), new Dimension(1280, 400));
         Image myImage = null;
         try {
-            myImage = ImageIO.read(getClass().getResource("backgrounds/coollogo.gif"));
+            myImage = ImageIO.read(getClass().getResource("backgrounds/TerraGen.png"));
             ImageIcon myImageIcon = new ImageIcon(myImage);
 
             JLabel label = new JLabel(myImageIcon);
